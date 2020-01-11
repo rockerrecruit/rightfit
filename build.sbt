618ef289d8487fe9f75672e8e4329280ca9ed401
Truong Hoang <truong.hoang@bynk.se>
2020-01-11 15:40:04 +0100

    WIP

--- build.sbt
+++ build.sbt
@@ -8,6 +8,9 @@ val ZioVersion = "1.0.0-RC17"
 
 val Http4sVersion = "0.21.0-M6"
 val DoobieVersion = "0.8.8"
+val ZIOVersion = "1.0.0-RC17"
+val PureConfigVersion = "0.11.0"
+val H2Version = "1.4.199"
 
 libraryDependencies ++= List(
   "dev.zio" %% "zio" % ZioVersion,
@@ -17,6 +20,9 @@ libraryDependencies ++= List(
   "org.http4s" %% "http4s-circe" % Http4sVersion,
   "org.http4s" %% "http4s-dsl" % Http4sVersion,
   "org.tpolecat" %% "doobie-core" % DoobieVersion,
-  "org.tpolecat" %% "doobie-h2" % DoobieVersion
+  "org.tpolecat" %% "doobie-h2" % DoobieVersion,
+  "com.github.pureconfig" %% "pureconfig" % PureConfigVersion,
+  "com.h2database" % "h2" % H2Version,
+  "org.slf4j" % "slf4j-log4j12" % "1.7.26"
 )
 
---
+++ src/main/scala/com/rightfit/api/Config.scala
@@ -0,0 +1,34 @@
+package com.rightfit.api
+
+case class Config(api: ApiConfig, dbConfig: DbConfig)
+case class ApiConfig(endpoint: String, port: Int)
+case class DbConfig(url: String, user: String, password: String)
+
+import zio.{RIO, Task}
+
+trait Configuration extends Serializable {
+  val config: Configuration.Service[Any]
+}
+
+object Configuration {
+  trait Service[R] {
+    val load: RIO[R, Config]
+  }
+
+  trait Live extends Configuration {
+    val config: Service[Any] = new Service[Any] {
+
+      val load: Task[Config] = Task.effect(loadConfigOrThrow[Config])
+    }
+  }
+
+  object Live extends Live
+
+  trait Test extends Configuration {
+    val config: Service[Any] = new Service[Any] {
+      val load: Task[Config] = Task.effectTotal(
+        Config(ApiConfig("loacalhost", 8080), DbConfig("localhost", "", "")))
+    }
+  }
+
+}
